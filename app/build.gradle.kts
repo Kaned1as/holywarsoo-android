0b9e6ebf7d62bef35f54aef944965fa75dab450b
Kanedias <kanedias@xaker.ru>
2020-09-04 02:37:26 +0300

    [app-specific] Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         manifestPlaceholders = mapOf("mainHost" to "holywarsoo.net")
         minSdkVersion(21)
         targetSdkVersion(29)
-        versionCode = 23
-        versionName = "1.5.3"
+        versionCode = 24
+        versionName = "1.5.4"
         vectorDrawables.useSupportLibrary = true
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
